9d672733e0ed94c14f52246e7e429fc9fa7e4807
zeycko <>
2025-11-28 22:45:12 +0100

    basic features

---
+++ README.md
@@ -0,0 +1,28 @@
+## Setup
+
+### Prerequisistes
+
+- Python 3.11.9 or higher
+- Pipx 1.6.0 or higher ``pip install pipx``
+- Poetry 2.1.2 or higher ``pipx install poetry``
+
+### Installation
+
+- Open terminal in project root
+- Build package with poetry
+``poetry build``
+- Go to <project_path>/dist
+- Install package
+``pipx install dofus_alt_tab-X.X.X-py3-none-any.whl``
+
+## Usage
+
+Default keys:
+
+- F2: previous window
+- F3: next window
+- F4: update windows
+
+Activate script: ``dofus-alt-tab``
+
+Stop script: ``ctrl+c``
---
+++ src/dofus_alt_tab/__main__.py
@@ -0,0 +1,65 @@
+import keyboard
+import win32gui
+import win32process
+import psutil
+
+
+_NEXT = "f3"
+_PREVIOUS = "f2"
+_UPDATE = "f4"
+
+
+class WindowsManager:
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.windows = []
+        self.update()
+
+    def update(self, *args, **kwargs):
+        self.windows.clear()
+        win32gui.EnumWindows(self._enum_callback, None)
+        if self.verbose:
+            names = [title.split(" - ")[0].strip() for title in self.windows]
+            print(f"Found {len(self.windows)} accounts: {', '.join(names)}")
+
+    def _enum_callback(self, hwnd, extra):
+        tid, pid = win32process.GetWindowThreadProcessId(hwnd)
+        class_name = win32gui.GetClassName(hwnd)
+        if "ime" in class_name.lower():
+            return False
+        title = win32gui.GetWindowText(hwnd)
+        exe_name = psutil.Process(pid).name()
+        if "dofus" in exe_name.lower():
+            self.windows.append(title)
+        return True
+
+    def next(self, *args, **kwargs):
+        title = self.windows[0]
+        hwnd = win32gui.FindWindow(None, title)
+        if hwnd:
+            keyboard.press_and_release("alt")  # weird fix to focus fighting
+            win32gui.SetForegroundWindow(hwnd)
+            self.windows.append(self.windows.pop(0))
+
+    def previous(self, *args, **kwargs):
+        title = self.windows[-1]
+        hwnd = win32gui.FindWindow(None, title)
+        if hwnd:
+            keyboard.press_and_release("alt")
+            win32gui.SetForegroundWindow(hwnd)
+            self.windows.insert(0, self.windows.pop())
+
+
+def main():
+    window_manager = WindowsManager(verbose=True)
+
+    keyboard.on_press_key(_NEXT, window_manager.next)
+    keyboard.on_press_key(_PREVIOUS, window_manager.previous)
+    keyboard.on_press_key(_UPDATE, window_manager.update)
+
+    keyboard.wait()
+
+
+if __name__ == '__main__':
+    main()
