c306a2a0f4e7bded8a817db7dd59f4c50e93c208
zeycko <>
2025-12-04 21:41:02 +0100

    bugfix
    fixes previous window behaviour

--- src/dofus_alt_tab/__main__.py
+++ src/dofus_alt_tab/__main__.py
@@ -2,6 +2,7 @@ import keyboard
 import win32gui
 import win32process
 import psutil
+import logging
 
 
 _NEXT = "f3"
@@ -11,17 +12,21 @@ _UPDATE = "f4"
 
 class WindowsManager:
 
+    @property
+    def account_names(self):
+        return [title.split(" - ")[0].strip() for title in self.windows]
+
     def __init__(self, verbose=False):
         self.verbose = verbose
         self.windows = []
+        self.active = 0
         self.update()
 
     def update(self, *args, **kwargs):
         self.windows.clear()
         win32gui.EnumWindows(self._enum_callback, None)
         if self.verbose:
-            names = [title.split(" - ")[0].strip() for title in self.windows]
-            print(f"Found {len(self.windows)} accounts: {', '.join(names)}")
+            print(f"Found {len(self.windows)} accounts: {', '.join(self.account_names)}")
 
     def _enum_callback(self, hwnd, extra):
         tid, pid = win32process.GetWindowThreadProcessId(hwnd)
@@ -37,18 +42,16 @@ class WindowsManager:
     def next(self, *args, **kwargs):
         title = self.windows[0]
         hwnd = win32gui.FindWindow(None, title)
-        if hwnd:
-            keyboard.press_and_release("alt")  # weird fix to focus fighting
-            win32gui.SetForegroundWindow(hwnd)
-            self.windows.append(self.windows.pop(0))
+        keyboard.press_and_release("alt")  # weird fix to focus fighting
+        win32gui.SetForegroundWindow(hwnd)
+        self.windows.append(self.windows.pop(0))
 
     def previous(self, *args, **kwargs):
+        self.windows.insert(0, self.windows.pop())
         title = self.windows[-1]
         hwnd = win32gui.FindWindow(None, title)
-        if hwnd:
-            keyboard.press_and_release("alt")
-            win32gui.SetForegroundWindow(hwnd)
-            self.windows.insert(0, self.windows.pop())
+        keyboard.press_and_release("alt")
+        win32gui.SetForegroundWindow(hwnd)
 
 
 def main():
